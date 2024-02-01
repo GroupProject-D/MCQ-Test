23160268b1630a56b35affe547bc67e098f65306
GroupProject-D <158473601+GroupProject-D@users.noreply.github.com>
2024-02-02 00:32:45 +0530

    Update DetailsCalling.java

--- src/com/quizapp/velocity/exam/DetailsCalling.java
+++ src/com/quizapp/velocity/exam/DetailsCalling.java
@@ -2,7 +2,7 @@ package com.quizapp.velocity.exam;
 
 public class DetailsCalling {
 	public static void main(String[] args) {
-		System.out.println("Welcome !!!");
+		System.out.println("Welcome To Project !!!");
 		UserView op= new UserView();
 		op.showDetailsToUser();
 	}
