716c133acb67a5bb3d299cec41bd4a61a171fde6
dbagal <bagaldnyaneshwar226@gmail.com>
2024-02-02 00:31:03 +0530

    Changes

--- src/com/quizapp/velocity/exam/DetailsCalling.java
+++ src/com/quizapp/velocity/exam/DetailsCalling.java
@@ -2,8 +2,8 @@ package com.quizapp.velocity.exam;
 
 public class DetailsCalling {
 	public static void main(String[] args) {
+		System.out.println("Welcome !!!");
 		UserView op= new UserView();
 		op.showDetailsToUser();
-        
 	}
 }
